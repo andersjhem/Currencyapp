f1394c35f25f7d3e2b12afd4e078c9820dd00f4f
Anders Johansson <138579776+andersjhem@users.noreply.github.com>
2024-10-16 10:34:19 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Currency Converter App");
+        System.out.println("Choose an operation:");
+        System.out.println(" 1. Convert SEK to USD");
+        System.out.println(" 2. Convert USD to SEK");
+        System.out.println(" 3. Convert SEK to EURO");
+        System.out.println(" 4. Convert EURO to SEK");
+        System.out.println("exit (Exit the Converter)");
+        System.out.println("==========================");
+        }
     }
-}
